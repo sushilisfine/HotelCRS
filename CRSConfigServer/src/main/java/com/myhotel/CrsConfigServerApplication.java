e532afa854f60b986af50eecbfe62724951f0d51
Sushilkumar Yadav <Sushilkumar_Yadav@epam.com>
2021-01-25 11:48:05 +0530

    Config Microservice

--- CRSConfigServer/src/main/java/com/myhotel/CrsConfigServerApplication.java
+++ CRSConfigServer/src/main/java/com/myhotel/CrsConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.myhotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CrsConfigServerApplication {
 
 	public static void main(String[] args) {
